9d04f2b723ecafc891d34f90fe426ade4456844a
Yeminaike <adeyemiogunnaike02@gmail.com>
2023-11-03 09:07:55 +0100

    discount price of a commodity

---
+++ index.js
@@ -0,0 +1,44 @@
+ 
+// const natureEl = document.querySelector("#Practice").textContent="Perfect";
+
+// const study = document.querySelector(".Study").textContent="Education";
+
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const odds = numbers.filter((num) => num % 2 === 1);
+// console.log(odds);
+
+
+// const figures = [1,2,3,4,5,6,7,8,9,10]
+
+// const even = figures.filter((figure) =>  figure % 2 === 0);
+// console.log(even); 
+
+
+
+// const primeNumbers = [1,2,3,4,5,6,7,8,9,10]
+// const prime = primeNumbers.filter((PM) =>  primeNumbers )
+
+
+const costPrice = 150000;
+const sellingPrice = 102000;
+
+
+const discountPercent = (costPrice - sellingPrice)/ costPrice * 100;
+
+console.log(`The discount price of the Android13 is`, Math.round(discountPercent)+ '%')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
